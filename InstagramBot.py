e0be5def08d1fc1965a19d584080339d44ea8dfb
Asher McKenna <ashermckenna@Ashers-M1-Max-MacBook-Pro.local>
2023-12-04 10:39:17 -0500

    Update InstagramBot.py
    Creating outline for code

--- InstagramBot.py
+++ InstagramBot.py
@@ -1,4 +1,28 @@
-import os
-import instagrapy
-import moviepy
+# Imports
 
+
+# Interact with user to request country to make content from
+
+
+# Connect with ChatGPT AI to get summary of that country
+
+
+# Pass summary to ElevenLabs
+
+
+# Pass country name to AI Image generator
+
+
+# Store images in directory
+
+
+# Use MoviePy to create video and render
+
+
+# Login to Instagram
+
+
+# Use ChatGPT to write a caption and generate hastags
+
+
+# Post video to instagram
