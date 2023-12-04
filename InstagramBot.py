69d75aa4e54369622e92c86397068f9a8e0ec8b2
aemckenna <mr.ashermckenna@gmail.com>
2023-12-04 14:00:35 -0500

    content type

--- InstagramBot.py
+++ InstagramBot.py
@@ -1,21 +1,25 @@
 # Imports
 import openai
 
-# Interact with user to request country to make content from
-
-# Set your OpenAI API key here
-openai.api_key = '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'
-
 # Function to interact with GPT-3
+openai.api_key = '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'
 def chat_with_gpt(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-002",  # You can choose a different engine if needed
+        engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=150,  # Adjust max_tokens as needed
-        temperature=0.7,  # Adjust temperature as needed
+        max_tokens=150,
+        temperature=0.7,
         stop=None  # You can specify custom stop words/phrases
     )
     return response.choices[0].text.strip()
+
+# Interact with user to request country to make content from
+print("Would you like to make an image post or a reel?")
+post_type = input()
+
+if post_type == reel
+
+
 
 # Example usage
 user_input = "Tell me a joke."
