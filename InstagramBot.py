2b93401c2f0630e02300699d2a4371ef59d4e757
aemckenna <mr.ashermckenna@gmail.com>
2023-12-08 10:42:02 -0500

    openai key

--- InstagramBot.py
+++ InstagramBot.py
@@ -1,16 +1,16 @@
 # Imports
 import openai
-from openai import OpenAI
 from pathlib import Path
 import requests
 import os
+from google.cloud import texttospeech
 
 # Determined Topic (change this value to set Instagram bot to post differently)
 topic_mode = "country"
-client = OpenAI()
 
 # Function to interact with GPT-3
 openai.api_key = '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'
+
 def ChatGPT(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -23,6 +23,7 @@ def ChatGPT(prompt):
 
 # Function that returns true if the word is found
 def WordFinder(sentence, word):
+    
     # Breaking the sentence into words
     s = sentence.split(" ")
 
@@ -40,39 +41,57 @@ if not (WordFinder(post_type, "reel") or WordFinder(post_type, "image")):
     print(f'You entered {post_type} which is not a valid post type. Please try again.')
     exit()
 
-# Requesting post topic
+# Requesting post type
 print(f'What country would you like to make a {post_type} on?')
 content_topic = input()
 
 # Checking to confirm that it matches post topic mode
 topic_mode_check = f'Answer only "yes" or "no". Is {content_topic} a {topic_mode}?'
 topic_check_response = ChatGPT(topic_mode_check)
+print(topic_check_response)
 
-if topic_check_response.lower() == "yes":
+if topic_check_response.lower() == "yes" or "yes.":
     pass
-elif topic_check_response.lower() == "no":
+elif topic_check_response.lower() == "no" or "no.":
     print(f'Your chosen {topic_mode} is not a valid {topic_mode} for this bot. Please use a different one next time.')
     exit()
 else:
     print(f'{content_topic} is not a valid {topic_mode}. Please choose a different one next time.')
     exit()
 
-# Creating content based on user input
 if post_type == "reel":
+    # Set the environment variable to the path of your service account key file
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/ashermckenna/Documents/InstagraPy/InstagramPyBot/rapid-pivot-407416-88aae5fe7262.json"
 
-    # Generating script to pass on to ElevenLabs
-    video_script = f'Write a script for a 30 second reel on {content_topic} the {topic_mode}.'
-    topic_check_response = ChatGPT(video_script)
-    
-    # Using ChatGPT text to speech to download audio for reel
-    speech_file_path = Path(__file__).parent / (f"{content_topic}.mp3")
-    response = client.audio.speech.create(
-    model = "tts-1-hd",
-    voice = "onyx",
-    input = video_script
+    # Create a Text-to-Speech client
+    client_tts = texttospeech.TextToSpeechClient()
+
+    # Set the text to be synthesized
+    text_to_speak = f"Write a script for a 30 second reel on {content_topic} the {topic_mode}."
+    script = ChatGPT(text_to_speak)
+
+    # Select the voice and other parameters
+    voice_tts = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        name="en-US-Wavenet-D",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL,
     )
 
-    response.stream_to_file(speech_file_path)
+    audio_config_tts = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.LINEAR16
+    )
+
+    # Synthesize the speech
+    response_tts = client_tts.synthesize_speech(
+        input=texttospeech.SynthesisInput(text=script),
+        voice=voice_tts,
+        audio_config=audio_config_tts
+    )
+
+    # Save the audio to a file
+    audio_file_path = Path(__file__).parent / (f"{content_topic}_audio.mp3")
+    with open(audio_file_path, "wb") as audio_file:
+        audio_file.write(response_tts.audio_content)
 
     # Generating prompts to pass on to Dall-E-3
     video_script = f'Write a script for a 30 second reel on {content_topic} the {topic_mode}.'
@@ -82,16 +101,13 @@ if post_type == "reel":
     os.makedirs(content_topic, exist_ok=True)
 
     # Generating images that will then be animated for the reel
-    response = client.images.generate(
-    model = "dall-e-3",
-    prompt = "a white siamese cat",
-    size = "1080x1920",
-    qualit = "hd",
-    n = 1,
-    )
+    dalle3_image = openai.Image.create(
+        prompt=content_topic, 
+        n=4, 
+        size="1024x1024")
 
     # Getting URL and downloading image
-    image_url = response.data[0].url
+    image_url = dalle3_image.data[0].url
     image_data = requests.get(image_url).content
     image_name = f"{content_topic}_image1.png"
 
@@ -108,9 +124,16 @@ else:
     exit()
 
 # Use MoviePy to create a video and render
+# Add your MoviePy code here
 
 # Login to Instagram
+# Add your Instagram login code here
 
 # Use ChatGPT to write a caption and generate hashtags
+caption_prompt = f"Write a caption and generate hashtags for a {post_type} post on {content_topic}."
+caption_response = ChatGPT(caption_prompt)
 
 # Post video to Instagram
+# Add your Instagram post code here
+
+# End of the script
